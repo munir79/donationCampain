08ba1db44483011f26d3ddd4162db2040a3d6f3e
munir79 <jakirhossainmunir79@gmail.com>
2023-09-26 22:05:59 +0600

    added

--- src/Component/Donation.js
+++ src/Component/Donation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Donation = () => {
     return (
         <div>
-            
+          <h4> this is donation section </h4>  
         </div>
     );
 };
--- src/Component/Statistics.js
+++ src/Component/Statistics.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics = () => {
     return (
         <div>
-            
+            <h4> This is statics section</h4>
         </div>
     );
 };
